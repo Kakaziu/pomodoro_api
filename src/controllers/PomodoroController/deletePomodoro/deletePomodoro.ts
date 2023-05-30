1bbd2d4649a96fcd404a3eb67ee651ad00a0d2cb
Kauã <kaua.borba03@gmail.com>
2023-05-30 16:07:21 -0300

    feat: delete pomodoro controller

---
+++ src/controllers/PomodoroController/deletePomodoro/deletePomodoro.ts
@@ -0,0 +1,26 @@
+import { badRequest } from "../../../helpers/responseFunctions";
+import { Pomodoro } from "../../../models/Pomodoro";
+import { HttpRequest, HttpResponse, ResponseBodyError } from "../../protocol";
+import {
+  IDeletePomodoroController,
+  IDeletePomodoroRepository,
+} from "./protocol";
+
+export class DeletePomodoroController implements IDeletePomodoroController {
+  constructor(
+    private readonly deletePomodoroRepository: IDeletePomodoroRepository
+  ) {}
+  async handle(
+    httpRequest: HttpRequest<any>
+  ): Promise<HttpResponse<Pomodoro | ResponseBodyError>> {
+    const id = httpRequest?.params?.id;
+
+    if (!id) return badRequest("Missing Id");
+
+    const pomodoroDeleted = await this.deletePomodoroRepository.deletePomodoro(
+      id
+    );
+
+    return ok<Pomodoro>(pomodoroDeleted);
+  }
+}
--- src/controllers/PomodoroController/deletePomodoro/protocol.ts
+++ src/controllers/PomodoroController/deletePomodoro/protocol.ts
@@ -1,4 +1,11 @@
 import { Pomodoro } from "../../../models/Pomodoro";
+import { HttpRequest, HttpResponse, ResponseBodyError } from "../../protocol";
+
+export interface IDeletePomodoroController {
+  handle(
+    httpRequest: HttpRequest<any>
+  ): Promise<HttpResponse<Pomodoro | ResponseBodyError>>;
+}
 
 export interface IDeletePomodoroRepository {
   deletePomodoro(id: string): Promise<Pomodoro>;
