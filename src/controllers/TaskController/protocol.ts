dc79bc7eced37806a1baa859eefeac14b044fc07
Kauã <kaua.borba03@gmail.com>
2023-05-31 22:01:09 -0300

    create task controller

--- src/controllers/TaskController/protocol.ts
+++ src/controllers/TaskController/protocol.ts
@@ -1,8 +1,15 @@
 import { Task } from "../../models/Task";
+import { HttpRequest, HttpResponse, ResponseBodyError } from "../protocol";
 
 export interface CreateTaskParams {
   title: string;
   createFor: string;
+}
+
+export interface ICreateTaskController {
+  handle(
+    httpRequest: HttpRequest<CreateTaskParams>
+  ): Promise<HttpResponse<Task | ResponseBodyError>>;
 }
 
 export interface ICreateTaskRepository {
